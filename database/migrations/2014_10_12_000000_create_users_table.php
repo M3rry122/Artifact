00a1c9d55534d9ec226709c74515e6a5f19eb362
M3rry122 <ykohei46@outlook.jp>
2023-03-15 03:34:40 +0900

    login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->string('profile', 100);
+            $table->rememberToken();
             $table->timestamps();
         });
     }

2d00ea7ed6dd79e345832e46a369a2945873383d
M3rry122 <ykohei46@outlook.jp>
2023-03-14 22:45:35 +0900

    delete

--- resources/views/posts/tweet.blade.php
+++ resources/views/posts/tweet.blade.php
@@ -11,10 +11,24 @@
                     <p class='body'> 
                       <a href="/posts/{{ $post->id }}">{{ $post->body }}</a> 
                     </p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                         @csrf
+                         @method('DELETE')
+                     <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                    </form>
                 </div>
             @endforeach    
         </div>
         <div class='paeginate'>{{ $posts->links()}}</div>
         <a href='/posts/create'>create</a>
+        <script>
+            function deletePost(id) {
+                 'use strict'
+
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                 document.getElementById(`form_${id}`).submit();
+                }
+           }
+        </script>
     </body>
 </html>
